fdf7977510094c8877e404bde1fa31a87cf59554
Eldiyar Musabaev <musabaeveldiar@gmail.com>
2023-07-24 16:31:58 +0600

    seventh try fixing failure run in render.com

--- src/test/java/kg/musabaev/onlinetutorback/OnlineTutorBackApplicationTests.java
+++ src/test/java/kg/musabaev/onlinetutorback/OnlineTutorBackApplicationTests.java
@@ -1,9 +1,8 @@
 package kg.musabaev.onlinetutorback;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class OnlineTutorBackApplicationTests {
 
 	@Test
