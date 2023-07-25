c79b6ef9bab87132cf868591ea64f0bb0d73cf09
Eldiyar Musabaev <musabaeveldiar@gmail.com>
2023-07-25 23:12:27 +0600

    configure cors globally

--- src/main/java/kg/musabaev/onlinetutorback/controller/CategoryController.java
+++ src/main/java/kg/musabaev/onlinetutorback/controller/CategoryController.java
@@ -4,7 +4,6 @@ import kg.musabaev.onlinetutorback.model.Category;
 import kg.musabaev.onlinetutorback.repository.CategoryRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,6 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/categories")
-@CrossOrigin(originPatterns = "*")
 public class CategoryController {
 
 	private final CategoryRepo categoryRepo;
