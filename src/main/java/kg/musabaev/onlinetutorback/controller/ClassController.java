c220fd2a91d83ab0bd11733035234664db448900
Eldiyar Musabaev <musabaeveldiar@gmail.com>
2023-07-25 16:54:32 +0600

    add allowed hosts

--- src/main/java/kg/musabaev/onlinetutorback/controller/ClassController.java
+++ src/main/java/kg/musabaev/onlinetutorback/controller/ClassController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/classes")
+@CrossOrigin(originPatterns = "*")
 public class ClassController {
 
 	private final ClassService classService;
