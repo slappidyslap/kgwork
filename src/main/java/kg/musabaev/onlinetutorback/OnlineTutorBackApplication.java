b5e4b891e0016afa419924bd0f2490779a6a0824
Eldiyar Musabaev <musabaeveldiar@gmail.com>
2023-07-26 18:43:49 +0600

    change default class cover url

--- src/main/java/kg/musabaev/onlinetutorback/OnlineTutorBackApplication.java
+++ src/main/java/kg/musabaev/onlinetutorback/OnlineTutorBackApplication.java
@@ -12,7 +12,7 @@ import java.util.stream.Stream;
 @SpringBootApplication
 public class OnlineTutorBackApplication {
 
-	public static String defaultCoverUrl = "https://www.seekpng.com/png/full/186-1868460_parental-advisory-explicit-content-logo-png-tricouri-parental.png";
+	public static String defaultCoverUrl = "https://memepedia.ru/wp-content/uploads/2018/01/%D1%80%D0%B6%D1%83%D0%BC%D0%B5%D0%BD-%D1%81%D0%BC%D0%B0%D0%B9%D0%BB-%D0%B2-%D0%BE%D1%87%D0%BA%D0%B0%D1%85.jpg";
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineTutorBackApplication.class, args);
